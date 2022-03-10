ab6192365404c7aa42716b5fe6babd4bc235dfaa
mrosenov <mitkorosenov@live.com>
2022-03-10 04:45:58 +0200

    cleaning the index

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<footer class="row row-cols-5 py-5 my-5 border-top">
+    <div class="col">
+        <p class="text-muted">Website © 2022</p>
+    </div>
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+    </div>
+</footer>
+</div>
+<script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,49 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Document</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </div>
-</nav>
+<?php include ("includes/header.php"); ?>
+<?php include ("includes/navigation.php"); ?>
+
 <div class="container">
     <div class="row" style="margin-top: 5px;">
         <div class="col col-lg-8">
@@ -62,59 +19,11 @@
                             Author: Mitko | Published: 10/03/2022 | Comments: 2
                         </div>
                     </div>
-                    <div class="card" style="margin-top: 5px;">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Author: Mitko | Published: 10/03/2022 | Comments: 2
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
         <div class="col col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
-                            <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
-                            <div class="d-flex gap-2 w-100 justify-content-between">
-                                <div>
-                                    <h6 class="mb-0">List group item heading</h6>
-                                    <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
-                                </div>
-                                <small class="opacity-50 text-nowrap">now</small>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <?php include ("includes/sidebar.php"); ?>
         </div>
     </div>
-    <footer class="row row-cols-5 py-5 my-5 border-top">
-        <div class="col">
-            <p class="text-muted">Website © 2022</p>
-        </div>
-        <div class="col"></div>
-        <div class="col"></div>
-        <div class="col">
-            <h5>Section</h5>
-            <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-            </ul>
-        </div>
-    </footer>
-</div>
-<script src="js/bootstrap.bundle.js"></script>
-</body>
-</html>
+<?php include ("includes/footer.php"); ?>
