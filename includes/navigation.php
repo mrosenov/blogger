6bf71c266958b0d58de1f5ccce356c3e6434e65d
mrosenov <mitkorosenov@live.com>
2022-03-14 18:25:50 +0200

    limiting the posts words

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="index.php">CMS Project</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
@@ -18,6 +18,7 @@
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <?php
+
                             $query = "SELECT * FROM categories";
                             $result = mysqli_query($connection, $query);
                             while ($row = mysqli_fetch_assoc($result)){
@@ -25,6 +26,7 @@
                                 $cat_title = $row['catTitle'];
                                 echo "<li><a class='dropdown-item' href='category.php?cat=$CategoryID'>$cat_title</a></li>";
                             }
+
                         ?>
                     </ul>
                 </li>
